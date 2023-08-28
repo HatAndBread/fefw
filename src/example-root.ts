21f91824d2d6cdee31e57d1733ab808b1480ecbd
HatAndBread <jhume@baremetrics.com>
2023-08-29 07:16:42 +0900

    Clean

--- src/example-root.ts
+++ src/example-root.ts
@@ -1,36 +1,39 @@
-import { use } from "./use"
-import { exampleComponent } from "./components/example-component"
+import { use } from "./use";
+import { exampleComponent } from "./components/example-component";
 
-const app = document.getElementById("app") as HTMLElement
+const app = document.getElementById("app") as HTMLElement;
 
-use(app, template, { count: 0 })
+use(app, template, { count: 0 });
 
 function template({ _, setState, stateFor, elements }: TemplateOptions) {
-  const { div, button } = elements
+  const { div, button } = elements;
 
   const handleButtonClick = (e: Event) => {
-    const currentValue = stateFor("count")
-    setState("count", currentValue + 1)
-  }
+    const currentValue = stateFor("count");
+    setState("count", currentValue + 1);
+  };
 
-  return div({ text: "hello", class: "bg-green", "data-stuff": "hello" }, _ => {
-    div({ text: "hello1" }, _ => {
-      div({ text: "hello2" }, _ => {
-        div({ text: "hello3" })
-        div({ text: "hello4" })
-        div({ text: "hello4" })
-        div({ text: "hello5" }, _ => {
-          div({ text: {count: (count: string) => `The count is ${count}`}})
-          button({ text: "I am a button", onclick: handleButtonClick })
-        })
-        ;[1, 2, 3, 4, 5].forEach(n => {
-          if (!(n % 2)) {
-            div({ text: n, class: n })
-          }
-        })
-      })
-      use(_, exampleComponent, { count: 3 })
-    })
-    div({ text: "last" })
-  })
+  return div(
+    { text: "hello", class: "bg-green", "data-stuff": "hello" },
+    (_) => {
+      div({ text: "hello1" }, (_) => {
+        div({ text: "hello2" }, (_) => {
+          div({ text: "hello3" });
+          div({ text: "hello4" });
+          div({ text: "hello4" });
+          div({ text: "hello5" }, (_) => {
+            div({ text: { count: (_) => `The count is ${_}` } });
+            button({ text: "I am a button", onclick: handleButtonClick });
+          });
+          [1, 2, 3, 4, 5].forEach((n) => {
+            if (!(n % 2)) {
+              div({ text: n, class: n });
+            }
+          });
+        });
+        use(_, exampleComponent, { count: 3 });
+      });
+      div({ text: "last" });
+    }
+  );
 }
